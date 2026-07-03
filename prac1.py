a8e97e0bfa11b6bf96ee0fd211aec86eab6286ac
Prathamshetty318 <110841240+Prathamshetty318@users.noreply.github.com>
2026-07-03 16:19:15 +0530

    done with my prac 1

--- prac1.py
+++ prac1.py
@@ -1,2 +1,8 @@
-print ("Hello")
-print ("Praham")
+name="
+age=18
+learning_python=True 
+print("My name is",name,"I am",age,"I am Learning_python", learning_python)
+print(type(name))
+print(type(age))
+print(type(learning_python))
+
