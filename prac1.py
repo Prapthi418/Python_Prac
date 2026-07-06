c6b553a759d78bd50a81fecfe2cbc672e2a0d5a5
Prathamshetty318 <110841240+Prathamshetty318@users.noreply.github.com>
2026-07-06 14:16:49 +0530

    i completed my practical2

--- prac1.py
+++ prac1.py
@@ -1,4 +1,4 @@
-name="
+name="Pratham"
 age=18
 learning_python=True 
 print("My name is",name,"I am",age,"I am Learning_python", learning_python)
@@ -6,3 +6,46 @@ print(type(name))
 print(type(age))
 print(type(learning_python))
 
+name="Pratham" 
+print(len(name))
+
+print(name[0])
+
+print(name[3])
+
+print(name[-1])
+
+print(name[-2])
+
+print(name[0:4])
+
+print(name[2:])
+
+print(name[:5])
+
+print(name[::-1])
+text="Python" 
+print(text*2+"3")
+print("5"+"5")
+print("5"*5)
+print(5*5)
+name=input("Enter your name")
+print(len(name))
+print(name[0])
+print(name[-1])
+print(name.upper())
+print(name.lower())
+print(name[::-1])
+print(name[1:5])
+
+print(name[:])
+
+print(name[::2])
+
+print(name[1::2])
+
+print(name[-4:-1])
+
+print(name[-1:-4])
+
+print(name[-1:-4:-1])
