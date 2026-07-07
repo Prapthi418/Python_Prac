1f9ac0eda529e0fbb90bfad470c55e9194b80933
Prathamshetty318 <110841240+Prathamshetty318@users.noreply.github.com>
2026-07-07 22:21:16 +0530

    slicing and list

---
+++ list.py
@@ -0,0 +1,19 @@
+word = "Programming" 
+print(word[3:8])
+print(word[::3])
+print(word[-5:])
+print(word[:-4])
+print(word[::-2])
+
+
+fruits=["Apple","Banana","Orange"]
+print(fruits)
+print(fruits[0])
+print(fruits[-1])
+print(len(fruits))
+fruits.append("Mango")
+print(fruits)
+fruits[1]="Pineapple"
+print(fruits)
+fruits.remove("Orange")
+print(fruits)
