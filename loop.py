8ac48a17e7d41c2535afb5a133953e7466c7a8f7
Prathamshetty318 <110841240+Prathamshetty318@users.noreply.github.com>
2026-07-11 21:00:36 +0530

    loop1

--- loop.py
+++ loop.py
@@ -1,4 +1,44 @@
 fruits=["Apple","Mango","Banana","Orange"] 
 
 for fruit in fruits: 
+    print(fruit) 
+fruits=[] 
+
+for fruit in fruits:
+    print(fruit) 
+
+fruits=[]
+
+for fruit in fruits: 
+    print("Hello") 
+
+fruits=["Apple","Banana","Orange"] 
+
+for fruit in fruits: 
     print(fruit)
+
+fruits=["Apple","Banana","Mango"] 
+
+for i in range(len(fruits)): 
+    print(i) 
+
+numbers=[5,10,15]
+
+for i in range(len(numbers)): 
+    print(numbers[i]*2) 
+
+numbers=[5,10,15] 
+
+for i in range(len(numbers)): 
+    numbers[i]=numbers[i]*2 
+    print(numbers) 
+
+
+
+numbers=[1,2,3] 
+ 
+for i in range(len(numbers)):
+    numbers[i] = numbers[i] * 10  
+
+print(numbers)  
+
